95c221369ce35cfe87b3f40bdd92b1258528a541
Dmitry Koka <dmitrykoka@gmail.com>
2020-03-27 13:43:04 +0200

    Added responsive design

--- src/components/LinkRoutes/LinkRoutes.js
+++ src/components/LinkRoutes/LinkRoutes.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./link-routes.css";
+import Menu from "../../assets/images/icons/Menu";
 
 const LinkRoutes = ({isHeaderMoved}) => {
 
   const path = window.location.pathname;
   const active = (params) => path.slice(-1) === params;
-
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(!open);
   return (
     <nav>
-      <ul className="nav">
+      <button onClick={handleOpen} className="burger-menu">
+
+      <Menu fill={isHeaderMoved ? "#fff" : "#000"}  width="30px" height="30px" />
+      </button>
+
+      <ul style={open && window.innerWidth < 669 ? {opacity: "0"} : {opacity: "1"}} className="nav">
         <li className="nav__item">
           <Link className={active("/") ?
               isHeaderMoved ? "nav__item--link--active" : "nav__item--link--active-scroll"
